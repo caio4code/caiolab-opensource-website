ac0994d850b1aa9d1221c9f288f865fdccc6dc79
Caio César <br.caiorecife@cesarleitao.com.br>
2019-08-06 22:50:54 -0300

    files

--- _data/_scripts/main.js
+++
@@ -1,8 +0,0 @@
-function corpyright(){
-var text = document.getElementById("corpyright");
-var data = new Date();
-var year = data.getFullYear();
-
-
-text.innerHTML = `Corpyright &copy${year} Caio César`;
-}
---
+++ _scripts/darkmode.js
@@ -0,0 +1,33 @@
+function darkmode(){
+    body = document.getElementById("body");
+    header = document.getElementById("header");
+    footer = document.getElementById("footer");
+
+    moon = document.getElementById("moon");
+    sun = document.getElementById("sun");
+
+    body.style.backgroundColor = "#272727";
+    body.style.color = "#fff";
+    header.style.backgroundColor = "#121212";
+    footer.style.backgroundColor = "#121212";
+
+    moon.style.display = "none";
+    sun.style.display = "block";
+}
+
+function lightmode(){
+    body = document.getElementById("body");
+    header = document.getElementById("header");
+    footer = document.getElementById("footer");
+
+    moon = document.getElementById("moon");
+    sun = document.getElementById("sun");
+
+    body.style.backgroundColor = "#ddd";
+    body.style.color = "#000";
+    header.style.backgroundColor = "#fff";
+    footer.style.backgroundColor = "#fff";
+
+    moon.style.display = "block";
+    sun.style.display = "none";
+}
