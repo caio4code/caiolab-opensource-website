1d9c4b5b43517d70124f99f2d6735c81a0ff9f32
Caio César <br.caiorecife@cesarleitao.com.br>
2019-08-30 11:11:56 -0300

    files

--- _scripts/main.js
+++ _scripts/main.js
@@ -6,3 +6,13 @@ var year = data.getFullYear();
 
 text.innerHTML = `Corpyright &copy${year} Caio César`;
 }
+
+
+function nabeka(){
+    var text = document.getElementById("sub-footer-core");
+    var data = new Date();
+    var year = data.getFullYear();
+    
+    
+    text.innerHTML = `Corpyright &copy${year} Nabeka | formas de pagamento: <i class="fab fa-cc-paypal"></i>`;
+}
